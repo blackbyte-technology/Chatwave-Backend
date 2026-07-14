31acc5d4cd9f0150707691b0ce471bd2de524853
rajdoescode <rajpatel01817@gmail.com>
2026-07-14 15:52:00 +0530

    feat: initialize environment-based MongoDB configuration settings

--- config/config.js
+++ config/config.js
@@ -2,6 +2,9 @@ import 'dotenv/config';
 
 export default {
   development: {
-    mongoUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/wapi'
+    mongoUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/chatwave'
+  },
+  production: {
+    mongoUri: process.env.MONGODB_URI
   },
 };
